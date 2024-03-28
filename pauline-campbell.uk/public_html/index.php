c2f896bf01f8fc9b3cdc926690581aeed39327f0
hermeneutika <hermeneutika@msn.com>
2024-03-28 13:07:14 +0000

    28/3/24 1307

--- pauline-campbell.uk/public_html/index.php
+++ pauline-campbell.uk/public_html/index.php
@@ -1,11 +1,56 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+<?php
+
+include("counter.php");
+
+?>
+
+<title>In memory of Pauline Campbell</title>
+<style type="text/css">
+h2
+{
+position:absolute;
+left:100px;
+top:150px;
+color:white;
+}
+
+</style>
+
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-20486261-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-20486261-1');
+</script>
+
 </head>
-<body>
-  <h1>testing</h1>
+
+<body style="background-color:black;">
+<h2>
+In memory of Pauline Campbell
+her daughter Sarah
+and Amrit Bhandari
+And not forgetting Karinne, Daniel, Michelle,Stuart Able and Flossy all victims of heroin.....(or their own personal choices?)
+</h2>
+<p></p>
+<A HREF="mailto:hermeneutika@msn.com">
+please feel free to email me</A>
+
+
+
+
 </body>
 </html>
+
+
+
+
+
+

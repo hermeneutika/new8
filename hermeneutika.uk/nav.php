2b4944c46eede2924220f007dee4c6a6dc1f415e
hermeneutika <hermeneutika@msn.com>
2024-04-11 08:43:37 +0100

    11/4/24 0843

--- hermeneutika.uk/nav.php
+++ hermeneutika.uk/nav.php
@@ -3,18 +3,19 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/nav-me.css" />
   <title>Document</title>
 </head>
 <body>
 <nav class="main-nav">
         <ul class="main-nav-list">
-          <li><a class="main-nav-link" href="#how">How it works</a></li>
-          <li><a class="main-nav-link" href="#meals">Meals</a></li>
+          <li><a class="parent" href="#1">One</a></li>
+          <li><a class="parent" href="#2">Two</a></li>
           <li>
-            <a class="main-nav-link" href="#testimonials">Testimonials</a>
+            <a class="parent" href="#3">Three</a>
           </li>
-          <li><a class="main-nav-link" href="#pricing">Pricing</a></li>
-          <li><a class="main-nav-link nav-cta" href="#cta">Try for free</a></li>
+          <li><a class="parent" href="#4">Four</a></li>
+          <li><a class="parent" href="#5">Five</a></li>
         </ul>
       </nav>
 

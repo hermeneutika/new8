80951abe43d54a8d15c2d1855e4981c8495749b2
hermeneutika <hermeneutika@msn.com>
2024-05-22 11:51:27 +0100

    22/5/24 1151

--- hermeneutika.uk/footer1.php
+++ hermeneutika.uk/footer1.php
@@ -1,16 +1,27 @@
 <footer class="footer">
 			<div class="container grid">
+				<div class="logo">
+				<a href="#">
+        <img class="logo" alt="Omnifood logo" src="img/omnifood-logo.png" />
+      </a>
+	  <p class="copyright">Copyright &copy: 2027 </p>
+</div>
 				<div class="address1">
 					<p class="chris">
-						Chair: Reverend Chris Whiteley 64 Kennedy Avenue Macclesfield SK10
-						3DE 01625 422404 07780 692808 cnwhiteley49@gmail.com
-					</p>
+						Chair: Reverend Chris Whiteley 64 Kennedy Avenue Macclesfield SK10 3DE</p>
+
+						<p class="tel"> 01625 422404 07780 692808</p>
+						 
+						
+					<p class="email:">cnwhiteley49@gmail.com</p>
 				</div>
 				<div class="address2">
 					<p class="steve">
 						Co-ordinator: Steve Oliver 1 Moorlands Close Macclesfield SK10 2TL
-						01625 421046 07801 715572 steve.oliver3@ntlworld.com
-					</p>
+</p>
+						<p class="tel:">01625 421046 07801 715572</p>
+						<p class="email:"> steve.oliver3@ntlworld.com</p>
+					
 				</div>
 			</div>
 		</footer>

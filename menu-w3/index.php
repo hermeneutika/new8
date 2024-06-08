50168eeb6d6373b8ad17e6a6c755d38e27340601
hermeneutika <hermeneutika@msn.com>
2024-06-08 08:52:55 +0100

    8/6/24 0852

--- menu-w3/index.php
+++ menu-w3/index.php
@@ -16,9 +16,31 @@
 
 <div class="topnav" id="myTopnav">
   <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
+  
+  <a class="main-nav-link" href="index.php">Home</a>
+					<a class="main-nav-link" href="#meals">Churches</a>
+					
+						<a class="main-nav-link" href="#testimonials">Gallery</a>
+					
+					<a class="main-nav-link" href="#pricing">Events</a>
+					
+						<a class="main-nav-link nav-cta" href="#cta"
+							>Strategic Priorities</a
+						>
+					
+					<a class="main-nav-link" href="orgs1.php">Organisations</a>
+					<a class="main-nav-link" href="purpose1.php">Purpose</a>
+					
+						<a class="main-nav-link" href="work1.php">How we work</a>
+					
+					<a class="main-nav-link" href="angel1.php">Street Angels</a>
+					
+						<a class="main-nav-link nav-cta" href="young.php">Young People</a>
+					
+					<a class="main-nav-link" href="praywalk1.php">Prayer Walking</a>
+					<a class="main-nav-link" href="eco1.php">Eco Church</a>
+
+					
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>

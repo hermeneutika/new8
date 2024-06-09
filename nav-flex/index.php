52296951de283672b119ea90d46d31d3a7b37bd4
hermeneutika <hermeneutika@msn.com>
2024-06-09 11:54:44 +0100

    9.6.24 1154

--- nav-flex/index.php
+++ nav-flex/index.php
@@ -3,7 +3,9 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/style-w3.css" />
+  <link rel="stylesheet" href="css/nav-flex.css" />
+  <!-- this is the link for icons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
   <title>Document</title>
 </head>
 <body>

34b9013734251f2858c849b9021d169b4ab69047
hermeneutika <hermeneutika@msn.com>
2024-07-01 13:21:23 +0100

    1/7/24 1321

---
+++ htdocs-old/thebritishdissenter.co.uk/public_html/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>The british dissenter</title>
+<link href="site.css" rel="stylesheet" type="text/css" />
+<!--last access 10/11/19  -->
+
+</head>
+<body>
+<?php include ("menu.php"); ?>
+<?php include ("link.php"); ?>
+<?php phpinfo(); ?>
+</body>
+</html>

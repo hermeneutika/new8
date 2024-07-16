9863e8d26b8dcbf819529d36b0bb0f7fa9c0dac7
hermeneutika <hermeneutika@msn.com>
2024-07-16 23:16:12 +0100

    16/7/24 2316

---
+++ all-hope/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="style1.css" />
+  <link rel="stylesheet" href="main.css" />
+  <title>Document</title>
+</head>
+<body>
+
+  <img class="logo" src="img/hope-logo.jpg" alt="hope-logo" />
+
+<?php include 'nav1.php'; ?>
+
+<!--<img class="slide1" src="slideshow/3.jpg" alt="hope-logo" /> -->
+<?php include 'slideshow1.htm'; ?>
+
+  <?php include 'footer1.php'; ?>
+</body>
+</html>
---
+++ all-hope/orgs1-new.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		
+    <link rel="stylesheet" href="main.css" />
+    <link rel="stylesheet" href="style1.css" />
+		<title>Document</title>
+	</head>
+	<body>
+  <img class="logo" src="img/hope-logo.jpg" alt="hope-logo" />
+	<?php include 'nav1.php'; ?>
+  
+  <embed src="orgs1.pdf" width=1300 height=300 />
+<?php include 'footer1.php'; ?>
+	</body>
+</html>

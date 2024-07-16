05c56f6119695402bc936f9e72d7407c1fee8f14
hermeneutika <hermeneutika@msn.com>
2024-07-16 16:54:45 +0100

    16/7/24 1655

--- public-domain-bibles/action_page.php
+++ public-domain-bibles/action_page.php
@@ -15,7 +15,8 @@ if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
 echo "Connected successfully";
-$sql="Select bible.text,ylt.text from bible,ylt where ylt.text  like '%wisdom'";
+$sql="select text from bible where text like '%wisdom%' union all select text from ylt where text like '%wisdom%'";
+//$sql="Select bible.text,ylt.text from bible,ylt where ylt.text  like '%wisdom'";
 //$sql = "SELECT n, book, chapt,verse,text FROM bible ";
 //$sql = "SELECT book,n,chapt,verse,text FROM bible,ylt WHERE text like '%wisdom%'";
 //$sql2="SELECT book,n,chapt,verse,text FROM ylt WHERE text like '%wisdom%'";
@@ -25,8 +26,11 @@ $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
+ // echo "number of rows=".$result;
   while($row = $result->fetch_assoc()) {
     echo "book: " . $row["n"]. " - Name: " . $row["chapt"]. " " . $row["verse"]. $row["text"]. "<br>";
+    echo "Book:" . $row["Book"]. "chapter" . $row["Chapter"]. "verse" . $row["verse"];
+    //echo "number of rows=".$result;
   }
 } else {
   echo "0 results";

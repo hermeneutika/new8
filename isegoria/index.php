c8f90bf56e96193a7e70e87b1b7379390ceb779a
hermeneutika <hermeneutika@msn.com>
2024-07-17 09:56:03 +0100

    17/7/24 0956

---
+++ isegoria/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="css/image.css" />
+		<title>Document</title>
+	</head>
+	<body>
+		<?php include 'menu-header.php'; ?>
+		
+		<section class="middle">
+			<div class="container">
+				<img class="bike1" src="img/bike1.jpg" alt="bike1" />
+			</div>
+		</section>
+
+		
+		<footer class="footer">
+			<div class="container grid">
+				<div class="address1">
+					<p class="chris">
+						Chair: Reverend Chris Whiteley 64 Kennedy Avenue Macclesfield SK10
+						3DE 01625 422404 07780 692808 cnwhiteley49@gmail.com
+					</p>
+				</div>
+				<div class="address2">
+					<p class="steve">
+						Co-ordinator: Steve Oliver 1 Moorlands Close Macclesfield SK10 2TL
+						01625 421046 07801 715572 steve.oliver3@ntlworld.com
+					</p>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>

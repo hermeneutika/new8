3a230f9b620b6da7c30f5966c924c44f9a81a0cd
hermeneutika <hermeneutika@msn.com>
2024-09-15 11:15:18 +0100

    15/9/24 1115

---
+++ theodicy.uk/menu1.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.codingnepalweb.com -->
+<html lang="en" dir="ltr">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Responsive Drop Down Navigation Menu | CodingLab</title>
+		<link rel="stylesheet" href="style.css" />
+		<!-- Boxicons CDN Link -->
+		<link
+			href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
+			rel="stylesheet" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<!--<?php include 'counter.php'; ?> -->
+		<title>Website Counter</title>
+  <script defer src="index.js"></script>
+  <link rel="stylesheet" href="counter.css
+	</head>
+	<body>
+		<nav>
+			<div class="navbar">
+				<i class="bx bx-menu"></i>
+				<div class="logo"><a href="#">HERMENEUTIKA</a></div>
+				<div class="nav-links">
+					<div class="sidebar-logo">
+						<span class="logo-name">HERMENEUTIKA</span>
+						<!--<?php include 'counter2.php'; ?> -->
+
+						<i class="bx bx-x"></i>
+					</div>
+					<ul class="links">
+						<li><a href="index.php">HOME</a></li>
+						<li>
+							<a href="input.php">Bible</a>
+							<i class="bx bxs-chevron-down htmlcss-arrow arrow"></i>
+							<ul class="htmlCss-sub-menu sub-menu">
+								<li><a href="news1.php">recent news</a></li>
+								<li><a href="#">hermeneutics</a></li>
+								<li><a href="#">theology</a></li>
+								<li class="more">
+									<span
+										><a href="#">More</a>
+										<i class="bx bxs-chevron-right arrow more-arrow"></i>
+									</span>
+									<ul class="more-sub-menu sub-menu">
+										<li><a href="#">Science</a></li>
+										<li><a href="philo1.php">Philosophy</a></li>
+										<li><a href="#">Sociology of knowledge</a></li>
+									</ul>
+								</li>
+							</ul>
+						</li>
+						<li>
+							<a href="#">Music</a>
+							<i class="bx bxs-chevron-down js-arrow arrow"></i>
+							<ul class="js-sub-menu sub-menu">
+								<li><a href="#">Totp Tunes</a></li>
+								<li><a href="#">live</a></li>
+								<li><a href="#">Electronics</a></li>
+								<li><a href="#">Videos</a></li>
+							</ul>
+						</li>
+						<li><a href="#">ABOUT US</a></li>
+						<li><a href="#">CONTACT US</a></li>
+					</ul>
+				</div>
+				<div class="search-box">
+					<i class="bx bx-search"></i>
+					<div class="input-box">
+						<input type="text" placeholder="Search..." />
+					</div>
+				</div>
+			</div>
+		</nav>
+		<script src="script.js"></script>
+	</body>
+</html>

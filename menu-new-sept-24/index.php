eb2b67c3ef278df13ec91597a6d5a7c442542a12
hermeneutika <hermeneutika@msn.com>
2024-09-16 16:35:18 +0100

    16/9/24 1635

--- menu-new-sept-24/index.php
+++ menu-new-sept-24/index.php
@@ -1,45 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.css" /> 
-  
-
-  <title>Document</title>
-  <title>Website Counter</title>
-  <script defer src="index.js"></script>
-  <link rel="stylesheet" href="counter.css" 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" /> 
+    <title>Document</title>
 </head>
 <body>
-
-<nav>
-
-
+    
+</body>
+</html><nav>
   <ul class="menu">
-    <li class="logo"><a href="#">THEODICY</a></li>
-    <li>
-    <div>Website visit count:</div>
-  <div class="website-counter"></div>
-  <!--<button id="reset">Reset</button>-->
-    </li>
+    <li class="logo"><a href="#">Creative Mind Agency</a></li>
     <li class="item"><a href="#">Home</a></li>
     <li class="item"><a href="#">About</a></li>
     <li class="item has-submenu">
-      <a tabindex="0">PRINCIPLES</a>
+      <a tabindex="0">Services</a>
       <ul class="submenu">
-        <li class="subitem"><a href="#">LINKS</a></li>
-        <li class="subitem"><a href="#">NEWS</a></li>
-        <li class="subitem"><a href="#">PRINCIPLES</a></li>
-        <li class="subitem"><a href="#">THEOLOGY</a></li>
+        <li class="subitem"><a href="#">Design</a></li>
+        <li class="subitem"><a href="#">Development</a></li>
+        <li class="subitem"><a href="#">SEO</a></li>
+        <li class="subitem"><a href="#">Copywriting</a></li>
       </ul>
     </li>
     <li class="item has-submenu">
-      <a tabindex="0">SCIENCE</a>
+      <a tabindex="0">Plans</a>
       <ul class="submenu">
-        <li class="subitem"><a href="#">METHODOLGY</a></li>
-        <li class="subitem"><a href="#">EVIDENCE</a></li>
-        <li class="subitem"><a href="#">THEORY</a></li>
+        <li class="subitem"><a href="#">Freelancer</a></li>
+        <li class="subitem"><a href="#">Startup</a></li>
+        <li class="subitem"><a href="#">Enterprise</a></li>
       </ul>
     </li>
     <li class="item"><a href="#">Blog</a></li>
@@ -51,75 +40,53 @@
   </ul>
 </nav>
 <script>
-  const toggle = document.querySelector(".toggle");
+const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".menu");
 const items = document.querySelectorAll(".item");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-	if (menu.classList.contains("active")) {
-		menu.classList.remove("active");
-		toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
-	} else {
-		menu.classList.add("active");
-		toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
-	}
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+  } else {
+    menu.classList.add("active");
+    toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+  }
 }
 
 /* Activate Submenu */
 function toggleItem() {
-	if (this.classList.contains("submenu-active")) {
-		this.classList.remove("submenu-active");
-	} else if (menu.querySelector(".submenu-active")) {
-		menu.querySelector(".submenu-active").classList.remove("submenu-active");
-		this.classList.add("submenu-active");
-	} else {
-		this.classList.add("submenu-active");
-	}
+  if (this.classList.contains("submenu-active")) {
+    this.classList.remove("submenu-active");
+  } else if (menu.querySelector(".submenu-active")) {
+    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    this.classList.add("submenu-active");
+  } else {
+    this.classList.add("submenu-active");
+  }
 }
 
 /* Close Submenu From Anywhere */
 function closeSubmenu(e) {
-	if (menu.querySelector(".submenu-active")) {
-		let isClickInside = menu
-			.querySelector(".submenu-active")
-			.contains(e.target);
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
 
-		if (!isClickInside && menu.querySelector(".submenu-active")) {
-			menu.querySelector(".submenu-active").classList.remove("submenu-active");
-		}
-	}
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
+  }
 }
 /* Event Listeners */
 toggle.addEventListener("click", toggleMenu, false);
 for (let item of items) {
-	if (item.querySelector(".submenu")) {
-		item.addEventListener("click", toggleItem, false);
-	}
-	item.addEventListener("keypress", toggleItem, false);
+  if (item.querySelector(".submenu")) {
+    item.addEventListener("click", toggleItem, false);
+  }
+  item.addEventListener("keypress", toggleItem, false);
 }
 document.addEventListener("click", closeSubmenu, false);
 
-</script>
-
-<<<<<<< HEAD
-/* REST OF THE SITE */
-
-<div class="container">
-
-<div>1</div>
-<div>2</div>
-<div>3</div>
-<div>4</div>
-<div>5</div>
-<div>6</div>
-
-</div>
-
-
-
-=======
-  
->>>>>>> 4f285bb75036dd08134c76fc1ae2c8e621d927c2
-</body>
-</html>
+    </script>

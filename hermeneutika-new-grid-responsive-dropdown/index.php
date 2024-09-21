041401da8f538a6dc65c87224515049cf94aecf1
hermeneutika <hermeneutika@msn.com>
2024-09-21 15:40:58 +0100

    21/9/24 1540

--- hermeneutika-new-grid-responsive-dropdown/index.php
+++ hermeneutika-new-grid-responsive-dropdown/index.php
@@ -3,7 +3,9 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="index.css">
 <title>Website Counter</title>
   <script defer src="index.js"></script>
   <!--<link rel="stylesheet" href="counter.css" /> -->
@@ -12,8 +14,17 @@
 </head>
 <body>
   <?php include 'nav_count1.php' ?>
-  <p>hello world</p>
-  
+ 
+  <div class="container">
+      <div class="header"><p>home page</p>
+
+
+      </div>
+      <div class="panel left">Left panel</div>
+      <div class="mainbody">Main Body</div>
+      <div class="panel right content">Right panel</div>
+      <div class="footer">Footer</div>
+    </div>
 
 
 </body>
--- hermeneutika-new-grid-responsive-dropdown/nav_count1.php
+++ hermeneutika-new-grid-responsive-dropdown/nav_count1.php
@@ -1,7 +1,8 @@
 <div class="topnav" id="myTopnav">
   <a href="#home" class="active">hermeneutika</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
+  <a href="news1.php">News</a>
+  <a href="#news">Philosophy</a>
+  <a href="#news">Theology</a>
   <div class="dropdown">
     <button class="dropbtn">Dropdown 
       <i class="fa fa-caret-down"></i>
@@ -12,20 +13,43 @@
       <a href="#">Link 3</a>
     </div>
   </div> 
+  <a href="#contact">Music</a>
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown 
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </div> 
+  <div class="dropdown">
+    <button class="dropbtn">Bible
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Hermeneutics</a>
+      <a href="#">Apologetics</a>
+      <a href="#">Polemics</a>
+      <a href="#">Prophecy</a>
+    </div>
+  </div> 
   <a href="#about">About</a>
 
-  <div>Website visit count:</div>
-  <div class="website-counter color=white"></div>
+  <div>since 20/09/24 count:</div>
+
+  <div class="website-counter align-right color=white"></div>
   <!--<button id="reset">Reset</button>-->
   <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
 </div>
 
-<div style="padding-left:16px">
+<!--<div style="padding-left:16px">
   <h2>Responsive Topnav with Dropdown</h2>
   <p>Resize the browser window to see how it works.</p>
   <p>Hover over the dropdown button to open the dropdown menu.</p>
 </div>
-
+-->
 <script>
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -36,4 +60,3 @@ function myFunction() {
   }
 }
 </script>
-<h2>hello world</h2>

cbf2155c4f16e22e49adf3f05709465da0beb4ee
hermeneutika <hermeneutika@msn.com>
2024-09-28 13:25:36 +0100

    28/9/24 1325

--- hermeneutika-test-grid-sept-24/index.php
+++ hermeneutika-test-grid-sept-24/index.php
@@ -5,16 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="style.css">
   <title>Document</title>
 </head>
 <body>
 <body>
     <div class="container">
-      <div class="header">Header</div>
+      <div class="header">Header <?php include 'nav_count1.php' ?></div>
       <div class="panel left">Left panel</div>
-      <div class="mainbody">ok this is the main body text.....ALL MATERIAL THINGS ARE SUBJECT TO DECAY THEREFORE WORK OUT YOUR SALVATION WITH DILIGENCE
+      <div class="mainbody">ALL MATERIAL THINGS ARE SUBJECT TO DECAY THEREFORE WORK OUT YOUR SALVATION WITH DILIGENCE
 
-Guatama Buddha.</div>
+Guatama Buddha. <br> <br>    Only the dead have seen the end of war  attributed to Plato</div>
       <div class="panel right">Right panel</div>
       <div class="footer">Footer</div>
     </div>

382d715543eec0fda07dda7027a7bfd2584b2d8e
hermeneutika <hermeneutika@msn.com>
2024-09-29 11:02:30 +0100

    29/9/24 1102

--- hermeneutika-test-grid-sept-24/index.php
+++ hermeneutika-test-grid-sept-24/index.php
@@ -12,12 +12,26 @@
 <body>
     <div class="container">
       <div class="header">Header <?php include 'nav_count1.php' ?></div>
-      <div class="panel left">Left panel</div>
+      <div class="panel left">
+        CURRENT MUSIC VIDS
+      
+<video width="320" height="240" controls>
+  <source src="gunshow.mp4#t=0.1" type="video/mp4">
+</video>
+      
+      
+      Left panel</div>
       <div class="mainbody">ALL MATERIAL THINGS ARE SUBJECT TO DECAY THEREFORE WORK OUT YOUR SALVATION WITH DILIGENCE
 
-Guatama Buddha. <br> <br>    Only the dead have seen the end of war  attributed to Plato</div>
-      <div class="panel right">Right panel</div>
-      <div class="footer">Footer</div>
+Guatama Buddha. <br> <br>    Only the dead have seen the end of war  attributed to Plato
+
+
+<br> <br>Php 2:12    Therefore, my beloved, as you have always obeyed, not as in my presence only, but now much more in my absence, work out your own salvation with fear and trembling;
+</div>
+
+
+      <div class="panel right">CURRENT NEWS</div>
+      <div class="footer">CONTACT</div>
     </div>
   </body>
 </body>

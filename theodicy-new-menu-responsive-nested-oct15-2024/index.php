8a399e2b6bc43b1b64fe430adbc2d14b30b1820d
hermeneutika <hermeneutika@msn.com>
2024-10-16 07:51:56 +0100

    16/10/24 0751

---
+++ theodicy-new-menu-responsive-nested-oct15-2024/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="navbar.css">
+    <title>Document</title>
+    <!--https://www.geeksforgeeks.org/holy-grail-layout-with-css-grid/ 16/10/24 -->
+</head>
+<body>
+    <?php include 'navbar.php' ?>
+    <br>
+    <div class="parent">
+        <header>
+            This is header
+        </header>
+        <div class="left-sidebar">
+            I am left sidebar
+            
+      
+        </div>
+        <main>
+            I am main content
+        </main>
+        <div class="right-sidebar">
+            I am right sidebar and
+            I am having more text
+        </div>
+        <footer>I am footer</footer>
+    </div>
+</body>
+</html>

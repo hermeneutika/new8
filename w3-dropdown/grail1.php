d95fdf617b389946dc3c6752811ad2604c15f6f1
hermeneutika <hermeneutika@msn.com>
2024-10-20 14:58:47 +0100

    2-/10/24 1458

---
+++ w3-dropdown/grail1.php
@@ -0,0 +1,35 @@
+<!-- index.html  -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content=
+        "width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="grail.css" />
+    <title>Holy Grail Layout</title>
+</head>
+
+<body>
+    <div class="parent">
+        <header>
+            Theodicy and the problems of evil and suffering.
+        </header>
+        <div class="left-sidebar">
+            I am left sidebar
+
+            <img src="palestine1.webp" width="200px" height="200px" alt="Palestinian Delusion cover image">
+            <img src="myth1.jpeg" width="200px" height="200px" alt="mytho f mental illness image">
+        </div>
+        <main class="main">
+            The Word of God is the total solution
+        </main>
+        <div class="right-sidebar">
+            I am right sidebar and
+            I am having more text
+        </div>
+        <footer>I am footer</footer>
+    </div>
+</body>
+
+</html>
--- w3-dropdown/index.php
+++ w3-dropdown/index.php
@@ -1,90 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="style.css"
-<style>
-
-</style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="grail.css" />
 </head>
 <body>
-
-<div class="topnav" id="myTopnav">
-  <a href="#home" class="active">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <div class="dropdown">
-    <button class="dropbtn">Theodicy 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Augustine</a>
-      <a href="#">Irenaeus</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  
-  <div class="dropdown">
-    <button class="dropbtn">Philosophy
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Existentialism</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <div class="dropdown">
-    <button class="dropbtn">Sciences
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">High Energy Physics</a>
-      <a href="#">Mathematics</a>
-      <a href="#">Biology?</a>
-    </div>
-  </div> 
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown4 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <div class="dropdown">
-    <button class="dropbtn">Dropdown5 
-      <i class="fa fa-caret-down"></i>
-    </button>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </div> 
-  <a href="#about">About</a>
-  <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
-</div>
-
-<div style="padding-left:16px">
-  <h2>Theodicy the problem of evil and suffering</h2>
-  <p>Resize the browser window to see how it works.</p>
-  <p>Hover over the dropdown button to open the dropdown menu.</p>
-</div>
-
-<script>
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
-</script>
-
+    <?php include 'nav.php'; ?>
+    <?php include 'grail1.php'; ?>
 </body>
 </html>

ade32a268702d337584bdc3843b4fdcc4c321b78
hermeneutika <hermeneutika@msn.com>
2025-04-12 21:54:13 +0100

    12/4/25 2154

---
+++ theodicy-april12-25/pray1.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="CREATED" CONTENT="20250208;22213281">
+	<META NAME="CHANGED" CONTENT="20250209;7463290">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include 'nav.php'; ?>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5>Prayer</FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5>The
+most important section of the entire web site. Prayer. Perhaps. Most
+Christians dont seem to ask the question, what is prayer? They assume
+that they know what prayer is and that their method of doing it is
+correct. Maybe they are right? But what is prayer? And is there a
+right and and a wrong way to pray? A couple of Scriptures</FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5><FONT COLOR="#000000"><B>Ps
+66:18</B></FONT><FONT COLOR="#000000"><SPAN STYLE="font-weight: normal">
+ If I regard </SPAN></FONT><FONT COLOR="#000000"><U><B>iniquity</B></U></FONT><FONT COLOR="#000000"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">
+in my </SPAN></SPAN></FONT><FONT COLOR="#000000"><U><B>heart</B></U></FONT><FONT COLOR="#000000"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">,
+the Lord will not hear </SPAN></SPAN></FONT><FONT COLOR="#000000"><I><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">me</SPAN></SPAN></I></FONT><FONT COLOR="#000000"><SPAN STYLE="font-style: normal"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">:</SPAN></SPAN></SPAN></FONT></FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm; font-style: normal; font-weight: normal; text-decoration: none">
+<BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5><FONT COLOR="#000000"><B>Eze
+14:3</B></FONT><FONT COLOR="#000000"><SPAN STYLE="font-weight: normal">
+ Son of man, these men have set up their idols in their </SPAN></FONT><FONT COLOR="#000000"><U><B>heart</B></U></FONT><FONT COLOR="#000000"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">,
+and put the stumblingblock of their </SPAN></SPAN></FONT><FONT COLOR="#000000"><U><B>iniquity</B></U></FONT><FONT COLOR="#000000"><SPAN STYLE="text-decoration: none"><SPAN STYLE="font-weight: normal">
+before their face: should I be enquired of at all by them?</SPAN></SPAN></FONT></FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+</BODY>
+</HTML>
---
+++ theodicy-april12-25/tim-apol.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="AUTHOR" CONTENT="michael falconer">
+	<META NAME="CREATED" CONTENT="20241102;16560000">
+	<META NAME="CHANGEDBY" CONTENT="michael falconer">
+	<META NAME="CHANGED" CONTENT="20241102;16560000">
+	<META NAME="AppVersion" CONTENT="16.0000">
+	<META NAME="DocSecurity" CONTENT="0">
+	<META NAME="HyperlinksChanged" CONTENT="false">
+	<META NAME="LinksUpToDate" CONTENT="false">
+	<META NAME="ScaleCrop" CONTENT="false">
+	<META NAME="ShareDoc" CONTENT="false">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2.54cm }
+		P { margin-bottom: 0.21cm; direction: ltr; color: #000000; widows: 2; orphans: 2 }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" TEXT="#000000" DIR="LTR">
+<?php include 'nav.php'; ?>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+&ldquo;<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Words cannot
+begin to express how deeply sorry I am for the hurt that I have
+caused. There is no defense for what I did, and I look back on the
+person I became with as much disdain as many of you likely do.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>First, I apologize
+to my former wife and remarkable children for my appalling actions.
+There&rsquo;s not a single day that goes by where I don&rsquo;t wish
+I could undo the damage I caused, and out of respect for their wishes
+I will not discuss anything else about them (now or in the future). I
+also ask anyone reading this to promote healing for them by
+respecting their privacy and defending them from any negativity or
+anger which should be directed towards me. I was the sole offender
+and the only one to blame for everything that happened.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>To my family, I
+apologize for the trauma you faced and may still feel. This is an
+ongoing sentence many of you serve because of me. I am so sorry to my
+friends who were betrayed by everything I hid from them and all the
+hardships I caused people who used to work with me. Band mates, road
+crew, managers, attorneys, agents, label people, and more all had to
+suffer through many unexpected changes because of my actions. While
+they were dealing with the aftermath of my arrest, I responded toward
+many of them with bitterness that I should have directed toward
+myself. I know that I can&rsquo;t undo the animosity I brought their
+way, but I hope to mend what I can now as time goes on.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>To the people who
+looked up to me as an artist, I let you down in so many ways. I tried
+to show my best side to the public, while feeding an ugly growing
+monster behind closed doors. I wrote lyrics about the person I wanted
+to be rather than the person that I was. I was living a life that
+lacked empathy and viewed everything through a self-motivated lens.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>I cannot say for
+certain what life looks like going forward as so much is different
+now and I&rsquo;m still learning. Music always has and always will be
+a part of me, and has helped me get through the darkest parts of my
+journey. However, this apology is not a part of promoting anything.
+Rumors circulate, and that&rsquo;s something I&rsquo;ve learned to
+accept, but this apology is just that, an apology to everyone around
+me.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>I&rsquo;ve remained
+silent to the public since expressing remorse at my sentencing
+because time seemed like the best way to promote healing. Today marks
+the first opportunity to freely apologize without any motivation to
+gain favor from the courts, as I have now completed the entirety of
+my legal sentence (including the completion of all parole/probation
+requirements). Let it be clear that no amount of time served can
+right my wrongs. I do not feel deserving of a second chance and am
+not asking for anyone&rsquo;s trust. The way many people feel about
+me makes sense, and only time will tell if my future actions line up
+with my remorse, something I pray for every day. In the last five
+years, the ripple effect of all my actions has extended further than
+a written statement can address. Thus, I will continue to apologize
+in both words and actions moving forward.</FONT></FONT></P>
+<P STYLE="margin-top: 0.18cm; margin-bottom: 0.18cm; line-height: 100%">
+<FONT FACE="Times New Roman, serif"><FONT SIZE=3>Thank you for
+reading, Tim&rdquo;</FONT></FONT></P>
+<P STYLE="margin-bottom: 0.28cm"><BR><BR>
+</P>
+</BODY>
+</HTML>

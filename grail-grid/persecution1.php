805a528725bc0010b63657f076b2d8dd122e5787
hermeneutika <hermeneutika@msn.com>
2025-04-21 13:30:34 +0100

    21/4/25 1330

---
+++ grail-grid/persecution1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="CREATED" CONTENT="20250421;13122092">
+	<META NAME="CHANGED" CONTENT="20250421;13262442">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+		P.western { so-language: en-GB }
+		A:link { so-language: zxx }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include 'nav.php' ;?>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Christian
+persecution</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5><A HREF="https://www.vaticannews.va/en/church/news/2023-04/over-50000-christians-killed-in-nigeria-by-islamist-extremists.html">Over
+50,000 Christians killed in Nigeria by Islamist extremists - Vatican
+News</A> </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>One only
+has to surf the net to find out the horrific news about the mindless
+slaughter of Christians in places like Nigeria by Muslims. What would
+happen one wonders if 50000 Muslims were slaughtered? </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Proverbs
+24</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=2 STYLE="font-size: 11pt"><SPAN STYLE="font-weight: normal">11
+&para; </SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-weight: normal">
+If thou forbear to deliver </SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">them
+that are</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+drawn unto death, and </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">those
+that are</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+ready to be slain;</SPAN></SPAN></FONT></FONT></FONT></FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=2 STYLE="font-size: 11pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">12
+</SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+If thou sayest, Behold, we knew it not; doth not he that pondereth
+the heart consider </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">it</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">?
+and he that keepeth thy soul, doth </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">not</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+he know </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">it</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">?
+and shall </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">not</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+he render to </SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><I><SPAN STYLE="font-weight: normal">every</SPAN></I></FONT></FONT></FONT><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">
+man according to his works?</SPAN></SPAN></FONT></FONT></FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT COLOR="#000000"><FONT FACE="Segoe UI, sans-serif"><FONT SIZE=4 STYLE="font-size: 16pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">What
+is the solution for the murderous death cult that is called Islam? </SPAN></SPAN></FONT></FONT></FONT>
+</P>
+</BODY>
+</HTML>

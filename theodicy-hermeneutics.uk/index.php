e55445c8ed2cf60313d6c15eb532d27847454d98
michael <hermeneutika@msn.com>
2025-05-27 13:32:21 +0100

    27/5/25 1332

--- theodicy-hermeneutics.uk/index.php
+++ theodicy-hermeneutics.uk/index.php
@@ -32,11 +32,8 @@ Joh 3:7  Marvel not that I said unto thee, Ye must be born again.<br>
 18  Thorns also and thistles shall it bring forth to thee; and thou shalt eat the herb of the field;<br>
 19  In the sweat of thy face shalt thou eat bread, till thou return unto the ground; for out of it wast thou taken: for dust thou [art], and unto dust shalt thou return.<br>
 <br><br></div> -->
-<<<<<<< HEAD
-=======
 <img src="israel-endtimes.jpg" width="160px" height="180px" alt="isreal endtime" />
 <iframe  width="260" height="215" src="https://www.youtube.com/embed/Cge0xWd8_zc?si=pn1faJc9Xvz0NLWx start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
->>>>>>> bdf33464aa37d0a7f7ed414224dffbda70243f9f
 <iframe  width="460" height="315" src="https://www.youtube.com/embed/Cge0xWd8_zc?si=pn1faJc9Xvz0NLWx start=10" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmichael.falconer.5209%2Fposts%2Fpfbid0Lh5P8maUETni8z5rXRsaxkP3PoL1XiPEfRbEfYDRGaW6BbWEjPgZpDivoYwdgUsal&show_text=true&width=500" width="500" height="452" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
 

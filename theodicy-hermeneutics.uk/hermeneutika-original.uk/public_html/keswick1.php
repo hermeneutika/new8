f72649deb47dae4ba9a04079a7b78ebcbd5087e3
michael <hermeneutika@msn.com>
2025-10-13 19:50:08 +0100

    13/10/25 1950

---
+++ theodicy-hermeneutics.uk/hermeneutika-original.uk/public_html/keswick1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+</style>
+<link href="menu.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<?php include ("menu-main.php"); ?>
+<a href ="https://vkc.keswickministries.org/day/?d=20210803" style="color:white">Main Keswick Conf site</a>
+
+<h1>Keswick Conference</h1>
+<h1>First main Christian meeting since lockdown</h1>
+
+
+
+</body>
+</html>
---
+++ theodicy-hermeneutics.uk/playlist1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/index.css" />
+</head>
+<body>
+    <a href="https://www.youtube.com/embed/2La3TqqMjGQ?si=gK3nUEYUXxv4FqTM">Chaos 8 Is there a tomorrow? </a>
+
+
+
+ </body>
+ </html>   

838272a4504083463f8d4115af0056bd644ef38c
hermeneutika <hermeneutika@msn.com>
2025-10-19 11:11:10 +0100

    19/10/25 1111

--- theodicy-hermeneutics.uk/nav.php
+++ theodicy-hermeneutics.uk/nav.php
@@ -124,6 +124,7 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
+      <a href="rare1.php">Rare Earth</a>
       <a href="mind1.php">Mind "sciences"</a>
       <a href="#">High Energy Physics</a>
       <a href="#">Mathematics</a>

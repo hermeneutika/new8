b3576a029fb8fd2e35f298a86e20470db8312edc
hermeneutika <hermeneutika@msn.com>
2025-11-03 17:07:37 +0000

    3/11/25 1707

--- wordpress-fse/wp-content/themes/neve-fse/assets/js/build/design-pack-notice.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-data', 'wp-element', 'wp-primitives'), 'version' => 'c76dc65efe8558432498');
--- wordpress-fse/wp-content/themes/neve-fse/assets/js/build/welcome-notice.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '31fd133979a85563ba84');

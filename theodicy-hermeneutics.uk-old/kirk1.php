9624e0dda28db875083e5ab6f974ced48d199e58
michael <hermeneutika@msn.com>
2025-11-16 07:14:38 +0000

    16/11/25 0715

---
+++ theodicy-hermeneutics.uk-old/kirk1.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="CREATED" CONTENT="20251019;6450930">
+	<META NAME="CHANGED" CONTENT="20251019;7005313">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+		P.western { so-language: en-GB }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include "nav.php" ;?>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Sunday 19
+Oct 2025</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Charlie
+Kirk's murder</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Its now
+about six weeks since the Charlie Kirk murder. Prior to his murder I
+had never heard of him. I may or may not have watched the odd video
+but I don't recall doing so. I was horrified at the murder, and also
+horrified at some peoples reaction to the murder. Even as a teenager
+many decades ago we always said &ldquo;don't speak ill of the dead&rdquo;.
+But that now seems to have gone out of fashion, even in the Christian
+Church. Maybe I totally misunderstood Charlie Kirk, but I got the
+impression from him that he was into encouraging dialogue between
+opposing views. Also in one interview with Charlie Kirk he was asked
+the question about how he would like to be remembered. He responded
+by saying he would like to be remembered for having a strong bold
+Christian faith. </FONT>
+</P>
+</BODY>
+</HTML>
---
+++ theodicy-hermeneutics.uk/advent1.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="CREATED" CONTENT="20241201;11392182">
+	<META NAME="CHANGED" CONTENT="20241202;15474542">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include 'nav.php'; ?>
+<P STYLE="margin-bottom: 0cm"><FONT SIZE=5>Advent and the Second
+Coming</FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=3>But
+no true Christian has any right to count on dying. There is</FONT></FONT></P>
+<P ALIGN=LEFT STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=3>something
+that is more certain than death. There are some who will never</FONT></FONT></P>
+<P ALIGN=LEFT STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=3>die.
+Those who are alive and waiting for Christ when he comes, shall never</FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=3>taste
+of death.</FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5>(Joseph
+Seiss Apocalypse)</FONT></FONT></P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><FONT FACE="Times New Roman, serif"><FONT SIZE=5>Advent
+in the latin means coming or arrival. So of course this could mean
+the coming or arrival of Christ in His inititial incarnation. Ie the
+virgin birth. But then since the first incarnation has already
+happened we are not really anticipating his first incarnation. If we
+believe that Christ will indeed come again, then perhaps it is more
+logical or even reasonable to look forward towards His Second
+Coming?? Each has their own opinion. But then if we are talking about
+a Biblical feast why do we not celebrate other Biblical feasts? </FONT></FONT>
+</P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P STYLE="margin-bottom: 0cm"><BR>
+</P>
+</BODY>
+</HTML>

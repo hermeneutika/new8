d5777643ef995e5fdd3d70332171cfd5b39032d2
hermeneutika <hermeneutika@msn.com>
2026-04-26 10:37:32 +0100

    26/4/26 1039

--- mysite/wp-content/plugins/elementor/vendor/autoload.php
+++ mysite/wp-content/plugins/elementor/vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitbd99e1246673bd03eedbf2290aa843f1::getLoader();
+return ComposerAutoloaderInit53017212cf884e4c8ec7f90756eae70b::getLoader();

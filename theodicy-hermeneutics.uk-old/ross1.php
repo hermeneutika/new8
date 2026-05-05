09e2e8660f492568e6c4857ba9fb9f7993d66bac
michael <hermeneutika@msn.com>
2026-05-05 16:03:56 +0100

    5/5/26 1605

--- theodicy-hermeneutics.uk-old/ross1.php
+++ theodicy-hermeneutics.uk-old/ross1.php
@@ -4,50 +4,50 @@
 	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
 	<TITLE></TITLE>
 	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
-	<META NAME="CREATED" CONTENT="20260505;13442252">
-	<META NAME="CHANGED" CONTENT="20260505;14024003">
+	<META NAME="CREATED" CONTENT="20260505;16002759">
+	<META NAME="CHANGED" CONTENT="20260505;16005621">
 	<STYLE TYPE="text/css">
 	<!--
 		@page { margin: 2cm }
 		P { margin-bottom: 0.21cm }
+		P.western { so-language: en-GB }
 	-->
 	</STYLE>
 </HEAD>
 <BODY LANG="en-GB" DIR="LTR">
-<P>&lt;?php include &quot;nav.php&quot;; ?&gt; 
+<?php include "nav.php"; ?>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Spiritual
+Maturity 5 May 2026 Maggie Ross &ndash; Pillars of Flame</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
 </P>
-<P STYLE="margin-bottom: 0cm"><FONT SIZE=5>Spiritual Maturity 5 May
-2026 Maggie Ross &ndash; Pillars of Flame</FONT></P>
-<P STYLE="margin-bottom: 0cm"><BR>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
 </P>
-<P STYLE="margin-bottom: 0cm"><BR>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Spiritual
+maturity is a way of willingness for transformation, a way of kenotic
+being that is the heart of Christianity. People do not become
+spiritually mature by a spoken rite, by learning fund-raising
+formulas, management techniques, and theological arguments. God is
+living and active; so must religion be.</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
 </P>
-<P STYLE="margin-bottom: 0cm"><FONT SIZE=5>Spiritual maturity is a
-way of willingness for transformation, a way of kenotic being that is
-the heart of Christianity. People do not become spiritually mature by
-a spoken rite, by learning fund-raising formulas, management
-techniques, and theological arguments. God is living and active; so
-must religion be.</FONT></P>
-<P STYLE="margin-bottom: 0cm"><BR>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Ross,
+Maggie. Pillars of Flame: Power, Priesthood, and Spiritual Maturity .
+Church Publishing Incorporated. Kindle Edition. </FONT>
 </P>
-<P STYLE="margin-bottom: 0cm"><FONT SIZE=5>Ross, Maggie. Pillars of
-Flame: Power, Priesthood, and Spiritual Maturity . Church Publishing
-Incorporated. Kindle Edition. </FONT>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
 </P>
-<P STYLE="margin-bottom: 0cm"><BR>
-</P>
-<P STYLE="margin-bottom: 0cm"> <FONT SIZE=5>This focus on control
-means that not only does the well-being of the institution take
-priority over the well-being of its members and the message of the
-Gospel; it means also that those who adopt this stance avoid the
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>This focus
+on control means that not only does the well-being of the institution
+take priority over the well-being of its members and the message of
+the Gospel; it means also that those who adopt this stance avoid the
 suffering necessary to their own growth into God, and as a result
 they do not have the capacity for suffering-with necessary to
 discerning and enabling others&rsquo; growth into God.</FONT></P>
-<P STYLE="margin-bottom: 0cm"><BR>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
 </P>
-<P STYLE="margin-bottom: 0cm"><FONT SIZE=5>Ross, Maggie. Pillars of
-Flame: Power, Priesthood, and Spiritual Maturity . Church Publishing
-Incorporated. Kindle Edition. </FONT>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Ross,
+Maggie. Pillars of Flame: Power, Priesthood, and Spiritual Maturity .
+Church Publishing Incorporated. Kindle Edition. </FONT>
 </P>
 </BODY>
 </HTML>

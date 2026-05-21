3e28e89c83a138f66d96fc869393b41d470e45ff
hermeneutika <hermeneutika@msn.com>
2026-05-21 09:13:42 +0100

    21/5/26 0913

--- theodicy-hermeneutics.uk/nav.php
+++ theodicy-hermeneutics.uk/nav.php
@@ -71,7 +71,8 @@
       
 </button>
 <div class="dropdown-content">
-  <a href="king1.php">king of the sciences</a>
+<a href="heal1.php">Christian Healing</a>
+<a href="king1.php">king of the sciences</a>
   <a href="shining1.php">The Shining Stranger</a>
 
 </div>
@@ -119,6 +120,7 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
+      <a href="ezek1.php">Ezekiel</a>
       <a href="zionism1.php">Zionism</a>
     <a href="daniel1.php">Daniel</a>
     <a href="islam1.php">Islamic Eschatology</a>
@@ -161,6 +163,7 @@
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
+      <a href="tech1.php">Computer parts</a>
       <a href="dna1.php">DNA</a>
       <a href="vaccine1.php">The Vaccine</a>
       <a href="archeology1.php">Biblical Archeology</a>

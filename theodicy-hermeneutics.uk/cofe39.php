e392d411821672272c7c5dcb128ec049a846a12b
hermeneutika <hermeneutika@msn.com>
2026-07-06 16:58:16 +0100

    6/7/26 1658

---
+++ theodicy-hermeneutics.uk/cofe39.php
@@ -0,0 +1,125 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.16  (Win32)">
+	<META NAME="CREATED" CONTENT="20260703;14194536">
+	<META NAME="CHANGED" CONTENT="20260706;16503461">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+		P.western { so-language: en-GB }
+		A:link { so-language: zxx }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include "nav.php"; ?>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Friday 3
+July 2026 </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>The Thirty
+Nine Articles</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Most of my
+friends and colleagues would say that religion is the cause of all
+the wars in the world. Of course who really cares??!! So what if
+religion is the cause of all conflicts? Surely if the language of
+faith is not acceptable in public discourse, then surely only the
+language of science is the only acceptable language of public
+discourse, and of course, Science can only proceed with empirically
+validated statements, ie what used to be called the scientific
+method. So to make a statement like religion is the cause of all wars
+and conflict, one would have to prove and validate such a statement
+by recourse to what used to be called the evidence. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>However if
+one goes into the history of philosophy, then one finds something
+that I choose to call &ldquo;radical British empiricism&rdquo;....ie
+only statements that can be backed up by evidence are valid. So
+called &ldquo;Metaphysical&rdquo; statement are regarded as
+meaningless. But as has been pointed out somewhere it is not possible
+to validate by reference to the empirical data &ldquo;radical British
+empiricism&rdquo;. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>So the
+theory falls down.Hoist by  its own petard. In the so called hard
+sciences it is possible to proceed with reference to the empirical
+evidence. But when one comes to the so called humanities and of
+course theology and philosophy then the rules of proof and evidence
+are not so clear. One is left perhaps in a endless sea or even ocean
+of opinion. And of course most people demand to be believed without
+any evidence or proof. There is apparently no methodology for
+demonstrating the proof or lack thereof  of any metaphysical
+statements. If there is no objective way to prove or disprove
+metaphysical statements then my opinion is just as valid as Einsteins
+or Newtons. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>So then we
+are brought to logic. Famously exemplified by the Vulcans in the TV
+series Star Trek mainly by Spock and Tuvok. Logic has been around for
+thousands of years. Eg Aristotle.  </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>6 july
+2026 </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="font-style: normal; font-weight: normal"><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt">Agreed
+upon by the Archbishops and Bishops of both Provinces and the whole
+clergy in the Convocation holden at London in the year 1562 for the
+avoiding of diversities of opinions and for the establishing of
+consent touching true religion.</FONT></FONT></FONT></P>
+<P CLASS="western" ALIGN=LEFT STYLE="font-style: normal; font-weight: normal; widows: 2; orphans: 2">
+<FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt">Reprinted
+by command of His Majesty King Charles 1 with his royal declaration
+prefixed thereunto.</FONT></FONT></FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Article 6</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">Holy</SPAN></SPAN></FONT></FONT></FONT><FONT COLOR="#000000">&nbsp;</FONT><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">Scripture
+containeth all things necessary to salvation: so that whatsoever is
+not read therein, nor may be proved thereby, is not to be required of
+any man, that it should be believed as an article of the Faith, or be
+thought requisite or necessary to salvation. In the name of the holy
+Scripture we do understand those Canonical Books of the Old and New
+Testament, of whose authority was never any doubt in the Church.</SPAN></SPAN></FONT></FONT></FONT>
+</FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">And
+the other Books (as&nbsp;</SPAN></SPAN></FONT></FONT></FONT><EM><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt"><SPAN STYLE="font-weight: normal">Hierome</SPAN></FONT></FONT></FONT></EM><FONT COLOR="#000000">&nbsp;</FONT><FONT COLOR="#000000"><FONT FACE="Open Sans, calibri, arial, sans-serif"><FONT SIZE=4 STYLE="font-size: 13pt"><SPAN STYLE="font-style: normal"><SPAN STYLE="font-weight: normal">saith)
+the Church doth read for example of life and instruction of manners;
+but yet doth it not apply them to establish any doctrine; such are
+these following:</SPAN></SPAN></FONT></FONT></FONT> </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>So these
+are writings that are extra Biblical and yet are given as a way to
+stop all division and dissension.</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>There are
+also more modern pronouncements. Eg the Chicargo Statement of
+Biblical Inerrancy. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5><A HREF="https://www.facebook.com/AllianceofConfessingEvangelicals">https://www.facebook.com/AllianceofConfessingEvangelicals</A></FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+</BODY>
+</HTML>

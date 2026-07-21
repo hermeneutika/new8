8ab443786a2b01f6324b55e45719bd2422c3dd1b
hermeneutika <hermeneutika@msn.com>
2026-07-21 14:11:56 +0100

    21/7/26 1410

---
+++ theodicy-hermeneutics.uk/cofe39.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=windows-1252">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="OpenOffice 4.1.15  (Win32)">
+	<META NAME="CREATED" CONTENT="20260703;14194536">
+	<META NAME="CHANGED" CONTENT="20260703;14505274">
+	<STYLE TYPE="text/css">
+	<!--
+		@page { margin: 2cm }
+		P { margin-bottom: 0.21cm }
+		P.western { so-language: en-GB }
+	-->
+	</STYLE>
+</HEAD>
+<BODY LANG="en-GB" DIR="LTR">
+<?php include "nav.php" ?>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Friday 3
+July 2026 </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>The Thirty
+Nine Articles</FONT></P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><BR>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>Most of my
+friends and colleagues would say that religion is the cause of all
+the wars in the world. Of course who really cares??!! So what if
+religion is the cause of all conflicts? Surely if the language of
+faith is not acceptable in public discourse, then surely only the
+language of science is the only acceptable language of public
+discourse, and of course, Science can only proceed with empirically
+validated statements, ie what used to be called the scientific
+method. So to make a statement like religion is the cause of all wars
+and conflict, one would have to prove and validate such a statement
+by recourse to what used to be called the evidence. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>However if
+one goes into the history of philosophy, then one finds something
+that I choose to call &ldquo;radical British empiricism&rdquo;....ie
+only statements that can be backed up by evidence are valid. So
+called &ldquo;Metaphysical&rdquo; statement are regarded as
+meaningless. But as has been pointed out somewhere it is not possible
+to validate by reference to the empirical data &ldquo;radical British
+empiricism&rdquo;. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>So the
+theory falls down.Hoist by  its own petard. In the so called hard
+sciences it is possible to proceed with reference to the empirical
+evidence. But when one comes to the so called humanities and of
+course theology and philosophy then the rules of proof and evidence
+are not so clear. One is left perhaps in a endless sea or even ocean
+of opinion. And of course most people demand to be believed without
+any evidence or proof. There is apparently no methodology for
+demonstrating the proof or lack thereof  of any metaphysical
+statements. If there is no objective way to prove or disprove
+metaphysical statements then my opinion is just as valid as Einsteins
+or Newtons. </FONT>
+</P>
+<P CLASS="western" STYLE="margin-bottom: 0cm"><FONT SIZE=5>So then we
+are brought to logic. Famously exemplified by the Vulcans in the TV
+series Star Trek mainly by Spock and Tuvok. Logic has been around for
+thousands of years. Eg Aristotle.  </FONT>
+</P>
+</BODY>
+</HTML>
